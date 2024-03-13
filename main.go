ab495024308127cb566d617747c8597b99abf0b4
Colink02 <colinkinzel@gmail.com>
2024-03-12 22:23:28 -0500

    Cleaned up Code and fixed issue with client error due to missing packet length.

--- main.go
+++ main.go
@@ -3,92 +3,115 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"log"
 	"net"
+	"os"
+
+	"github.com/tailscale/hujson"
 )
 
-type AnyComponent struct {
-	Value interface{}
+type Config struct {
+	Verbose  bool `json:"verbose"`
+	Listener struct {
+		Host string `json:"host"`
+	} `json:"listener"`
+	Query query `json:"query"`
 }
 
 type query struct {
-	Version     queryVersion `json:"version"`
-	Players     players      `json:"players"`
-	Description AnyComponent `json:"description"`
-	Favicon     string       `json:"favicon"`
-}
-type queryVersion struct {
-	Name     string `json:"name"`
-	Protocol int    `json:"protocol"`
-}
-type players struct {
-	Max    int      `json:"max"`
-	Online int      `json:"online"`
-	Sample []player `json:"sample,omitempty"`
-}
-type player struct {
-	Name string `json:"name"`
-	Id   string `json:"id"`
+	Version struct {
+		Name     string `json:"name"`
+		Protocol int    `json:"protocol"`
+	} `json:"version"`
+	Players struct {
+		Max    int `json:"max"`
+		Online int `json:"online"`
+		Sample []struct {
+			Name string `json:"name"`
+			Id   string `json:"id"`
+		} `json:"sample,omitempty"`
+	} `json:"players"`
+	Description interface{} `json:"description"`
+	Favicon     string      `json:"favicon"`
 }
 
-type handshake struct {
-	protocolVersion int32
-	serverAddress   string
-	serverPort      uint16
-	nextState       int32
+type Handshake struct {
+	ProtocolVersion VarInt
+	ServerAddress   string
+	ServerPort      uint16
+	NextState       VarInt
 }
+
+var config = readConfig()
+
+// ------------ Editable Variables ------------
+var verbose = false
 
 func main() {
-	// Create a new UDP connection handler
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", "localhost:25567")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	listener, err := net.ListenTCP("tcp4", tcpAddr)
-	println("Server started on localhost:25567")
+	// Create a new UDP connection handler
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", config.Listener.Host)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error resolving TCP address: ", err.Error())
 	}
-	defer listener.Close()
+	listener, err := net.ListenTCP("tcp4", tcpAddr)
+	if err != nil {
+		log.Fatal("Error listening on TCP address: ", err.Error())
+	}
+	log.Println("Server started on ", listener.Addr().String())
+	defer func(listener *net.TCPListener) {
+		err := listener.Close()
+		if err != nil {
+			log.Println("Error closing listener: ", err.Error())
+			return
+		}
+	}(listener)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Println("Connection error: ", err)
+			log.Println("Connection error: ", err.Error())
 			return
 		}
 		go serve(conn)
 	}
-	println("Server is shutting down...")
 }
 
 func serve(conn net.Conn) {
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error closing connection: ", err.Error())
+			return
 		}
 	}(conn)
 	var state int32 = 0
 
 	for {
 		reader := bufio.NewReader(conn)
-		_, err := ReadVarInt(reader)
+		//Read the packet length
+		packetLength, err := ReadVarInt(reader)
 		if err != nil {
 			return
 		}
+		//Read the packet id
 		packetId, _ := ReadVarInt(reader)
-		println("Packet Id is: ", packetId)
+		if config.Verbose {
+			log.Println("Packet Length is: ", packetLength)
+			log.Println("Packet Id is: ", packetId)
+		}
 		// Read the incoming connection into the buffer.
 		if state == 0 {
 			if packetId == 0x00 {
-				println("Received a Handshake")
+				if config.Verbose {
+					log.Println("Received a Handshake")
+				}
 				var done bool
-				state, done = getHandshake(conn, state, *reader)
+				state, done = getHandshake(conn, *reader)
 				if done {
-					println("Done with handshake")
+					log.Println("State is now: ", state)
 				}
 			}
 		} else if state == 1 {
@@ -96,116 +119,164 @@ func serve(conn net.Conn) {
 				println("Received a Status Request")
 				// Status request
 				if sendStatus(conn) {
-					println("Sent Status")
+					if config.Verbose {
+						log.Println("Sent Status to ", conn.RemoteAddr().String())
+					}
 				}
 			} else if packetId == 0x01 {
 				println("Received a Ping Request")
 				// ping request
 				if sendPing(conn, *reader) {
-					println("Sent Ping")
+					if config.Verbose {
+						log.Println("Sent Ping to ", conn.RemoteAddr().String())
+					}
 				}
+			} else if packetId == 0xFE {
+				log.Println("Received a Legacy Ping Request")
 			}
 		}
 	}
 }
 
-func sendPing(conn net.Conn, reader bufio.Reader) bool {
-
-	packet := bytes.NewBuffer(make([]byte, 1024))
-	err := WriteVarInt(packet, VarInt(0x01))
+func getHandshake(conn net.Conn, reader bufio.Reader) (int32, bool) {
+	var handshake Handshake
+	var err error
+	// Read Protocol Version
+	handshake.ProtocolVersion, err = ReadVarInt(&reader)
 	if err != nil {
-		println(err)
-		return false
-	}
-	ping, _ := ReadVarLong(&reader)
-	err = WriteVarLong(packet, ping)
-	write, err := conn.Write(packet.Bytes())
-	println("Wrote ", write, " Bytes")
-	if err != nil {
-		return false
-	}
-	if err != nil {
-		println(err)
-		return false
-	}
-
-	return true
-}
-
-func getHandshake(conn net.Conn, state int32, reader bufio.Reader) (int32, bool) {
-	var protocolVersion VarInt
-	var serverAddress string
-	var serverPort uint16
-	var nextState VarInt
-	protocolVersion, err := ReadVarInt(&reader)
-	if err != nil {
-		println("Error ProtocolVersion", err)
+		log.Println("Error ProtocolVersion: ", err.Error())
 		return 0, false
 	}
-	println("Protocol Version: ", protocolVersion)
-	serverAddress, err = ReadString(&reader)
+	// Read Server Address
+	handshake.ServerAddress, err = ReadString(&reader)
 	if err != nil {
-		println("Error ServerAddress", err)
+		log.Println("Error ServerAddress: ", err.Error())
 		return 0, false
 	}
-	println("Server Address: ", serverAddress)
+	// Read Server Port
 	var tmp [2]byte
 	if _, err = reader.Read(tmp[:2]); err != nil {
-		println("Error ServerPort", err)
+		log.Println("Error ServerPort: ", err.Error())
 		return 0, false
 	}
-	serverPort = (uint16(tmp[1]) << 0) | (uint16(tmp[0]) << 8)
-	println("Server Port: ", serverPort)
-	nextState, err = ReadVarInt(&reader)
+	handshake.ServerPort = (uint16(tmp[1]) << 0) | (uint16(tmp[0]) << 8)
+	// Read Next State
+	handshake.NextState, err = ReadVarInt(&reader)
 	if err != nil {
-		println("Error nextState", err)
+		log.Println("Error nextState: ", err.Error())
 		return 0, false
 	}
-	println("Next State: ", nextState)
 
-	println("Got a handshake from ", conn.RemoteAddr().String())
-	state = int32(nextState)
-	return state, true
+	if config.Verbose {
+		log.Println("Got a handshake from ", conn.RemoteAddr().String())
+	}
+	return int32(handshake.NextState), true
 }
 
 func sendStatus(conn net.Conn) bool {
-	println("Got a packet from ", conn.RemoteAddr().String())
-
+	var packetData bytes.Buffer
 	var packet bytes.Buffer
-	err := WriteVarInt(&packet, VarInt(0))
-	if err != nil {
-		println(err)
-		return true
-	}
-	testQuery := &query{
-		Version: queryVersion{
-			Name:     "1.19.4",
-			Protocol: 762,
-		},
-		Players: players{
-			Max:    20,
-			Online: 10,
-		},
-		Description: AnyComponent{
-			"Super Epic Overridden Server Description!",
-		},
-	}
 
-	b, err := json.Marshal(&testQuery)
+	// Write Packet ID (0x00 for status response)
+	err := WriteVarInt(&packetData, VarInt(0))
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = WriteString(&packet, string(b))
-	println("String: ", string(b))
-	if err != nil {
-		println("Error", err.Error())
+		log.Println("Error writing VarInt to buffer", err.Error())
 		return false
 	}
+	// Parse json from query struct
+	b, err := json.Marshal(config.Query)
+	if err != nil {
+		log.Println("Error Parsing query to JSON: ", err.Error())
+		return false
+	}
+	// Write JSON string to packet buffer
+	err = WriteString(&packetData, string(b))
+	if err != nil {
+		log.Println("Error writing query string: ", err.Error())
+		return false
+	}
+	// Write packetData size to packet buffer
+	err = WriteVarInt(&packet, VarInt(packetData.Len()))
+	if err != nil {
+		log.Println("Error writing packetData size: ", err.Error())
+		return false
+	}
+	// Write packetData to packet buffer so that the size is before the rest of the data
+	_, err = packetData.WriteTo(&packet)
+	if err != nil {
+		log.Println("Error writing packetData to the packet: ", err.Error())
+		return false
+	}
+	// Write packet to connection
 	_, writeErr := conn.Write(packet.Bytes())
 	if writeErr != nil {
-		println(writeErr.Error())
+		log.Println("Error writing packet to connection: ", writeErr.Error())
 		return false
 	}
 	return true
+}
+
+func sendPing(conn net.Conn, reader bufio.Reader) bool {
+	var ping int64
+	var packet bytes.Buffer
+	var packetData bytes.Buffer
+	var err error
+	//Write packet id to packetData
+	err = WriteVarInt(&packetData, VarInt(1))
+	if err != nil {
+		log.Println("Error writing PacketId to buffer", err.Error())
+		return false
+	}
+	//Read ping long from client
+	err = binary.Read(&reader, binary.BigEndian, &ping)
+	if err != nil {
+		log.Println("Error reading ping from client", err.Error())
+		return false
+	}
+	//Write ping data from client to packetData
+	err = binary.Write(&packetData, binary.BigEndian, &ping)
+	if err != nil {
+		log.Println("Error writing ping to packetData", err.Error())
+		return false
+	}
+	//Set packetData size
+	err = WriteVarInt(&packet, VarInt(packetData.Len()))
+	if err != nil {
+		return false
+	}
+	//Write packetData to packetData
+	_, err = packetData.WriteTo(&packet)
+	if err != nil {
+		log.Println("Error writing packetData to packet", err.Error())
+		return false
+	}
+	//Write packet to connection
+	write, err := conn.Write(packet.Bytes())
+	if config.Verbose {
+		log.Println("Wrote ", write, " Bytes to ", conn.RemoteAddr().String())
+	}
+	if err != nil {
+		log.Println("Error writing packet to connection: ", err.Error())
+		return false
+	}
+	return true
+}
+
+func readConfig() Config {
+	f, err := os.ReadFile("config.jsonc")
+	if err != nil {
+		log.Fatal("Error opening config file: ", err.Error())
+	}
+
+	var cfg Config
+	parse, err := hujson.Parse(f)
+	if err != nil {
+		return Config{}
+	}
+	parse.Standardize()
+	err = json.Unmarshal(parse.Pack(), &cfg)
+	if err != nil {
+		log.Fatal("Error decoding config file: ", err.Error())
+	}
+	return cfg
 }
